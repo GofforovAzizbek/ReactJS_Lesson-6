82e29ad9183a69f5029d0e7de88d7f8f7a317dba
GofforovAzizbek <gofforovazizbek2024@gmail.com>
2026-03-01 08:39:34 +0500

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./assets/components/Header/Header";
+function App() {
+  return (
+    <>
+      {/* <header></header> */}
+      <Header />
+      <main></main>
+      <footer></footer>
+    </>
+  );
+}
+
+export default App;
---
+++ src/assets/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "../../../index.css";
+function Header() {
+  return (
+    <>
+      <header className="pt-[30px]">
+        <div className="container">salom</div>
+      </header>
+    </>
+  );
+}
+
+export default Header;
